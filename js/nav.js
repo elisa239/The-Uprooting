efc482f50aee908954e12c1c06086d132e28ce7b
elisawino92 <elisa.wino@gmail.com>
2015-12-17 19:31:22 +0100

    added sticky footer and header to stories

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
 	var mn = $(".main-nav");
 	    mns = "main-nav-scrolled";
 	    hdr = $('header').height();
+	    mf = $(".footer-inner");
+	    mfs = "footer-inner-scrolled";
+	    ftr = $('footer').height();
 	
 	$(window).scroll(function() {
 	  if( $(document).scrollTop() > hdr ) {
@@ -9,7 +12,11 @@ $( document ).ready(function() {
 	  } else {
 	    mn.removeClass(mns);
 	  }
+
+	  if( $(document).scrollTop() > ftr ) {
+	    $(mf).addClass(mfs);
+	  } else {
+	    mf.removeClass(mfs);
+	  }
 	});
 });
-
-
