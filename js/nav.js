843b22c602634ef01299272d3ed232bc6e155c12
elisawino92 <elisa.wino@gmail.com>
2015-12-19 23:17:51 +0100

    finished stories, started to work on article

--- js/nav.js
+++ js/nav.js
@@ -2,21 +2,22 @@ $( document ).ready(function() {
 	var mn = $(".main-nav");
 	    mns = "main-nav-scrolled";
 	    hdr = $('header').height();
-	    mf = $(".footer-inner");
+	    /*mf = $(".footer-inner");
 	    mfs = "footer-inner-scrolled";
-	    ftr = $('footer').height();
-	
+	    ftr = $('footer').height();*/
+
 	$(window).scroll(function() {
 	  if( $(document).scrollTop() > hdr ) {
 	    $(mn).addClass(mns);
 	  } else {
 	    mn.removeClass(mns);
 	  }
+	});
+});
 
-	  if( $(document).scrollTop() > ftr ) {
+	  /*if( $(document).scrollTop() > ftr ) {
 	    $(mf).addClass(mfs);
 	  } else {
 	    mf.removeClass(mfs);
 	  }
-	});
-});
+	});*/
